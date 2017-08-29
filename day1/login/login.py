a4821c611779479ccc0cdf1c2dfd11efb0abe708
hlr423 <1217402420@qq.com>
2017-08-30 00:28:51 +0800

    v0.20170830

--- day1/login/login.py
+++ day1/login/login.py
@@ -1,18 +1,21 @@
 lock = "lock.txt"
 logfile = "login.txt"
 lock_info = 0
+lock1_info = 0
 i = 0
 
-while i < 3 and lock_info== 0:
+while i < 3 and lock_info== 0 and lock1_info == 0 :
     name = input("Please input your name: ")
     password = input("Please input password: ")
 
     f = open(lock, "r")
-    if name in f:
-        lock_info = 1
-        # print("This %s is locked" % username)
-        f.close()
-        break
+    # print(f)
+    for line in f.readlines():
+        if name in line:
+            lock_info = 1
+            # print("This %s is locked" % username)
+            f.close()
+            break
     else:
         f = open(logfile, "r")
         for line in f.readlines():
@@ -22,14 +25,17 @@ while i < 3 and lock_info== 0:
             # print(pass_file)
             if user_file == name and pass_file == password:
                 print("Bingo!")
+                lock1_info = 1
                 break
         else:
             print("You name or password is errer!")
+            i += 1
         f.close()
-    i += 1
 else:
     if i == 3 or lock_info == 1:
+        print(i)
+        print(lock_info)
         f = open(lock, "a")
         f.write(name + "\n")
         f.close()
-    print("This %s is locked" % name)
+        print("This %s is locked" % name)
