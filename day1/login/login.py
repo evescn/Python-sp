c377a8aa08c0edcbceb8912356faca7bcfa07ffc
hlr423 <1217402420@qq.com>
2017-08-30 21:53:01 +0800

    v0.201708301

--- day1/login/login.py
+++ day1/login/login.py
@@ -1,41 +1,36 @@
+import sys
+
 lock = "lock.txt"
 logfile = "login.txt"
-lock_info = 0
-lock1_info = 0
+login_info = 0
 i = 0
 
-while i < 3 and lock_info== 0 and lock1_info == 0 :
+while i < 3 and login_info== 0 :
     name = input("Please input your name: ")
-    password = input("Please input password: ")
 
     f = open(lock, "r")
-    # print(f)
     for line in f.readlines():
-        if name in line:
-            lock_info = 1
-            # print("This %s is locked" % username)
+        # if name in line:
+        if name == line.strip():
             f.close()
+            sys.exit('\033[32:1m用户 %s 已经被锁定\033[0m' % name)
+
+    password = input("Please input password: ")
+
+    f = open(logfile, "r")
+    for line in f.readlines():
+        user_file, pass_file = line.split()
+        if user_file == name and pass_file == password:
+            print("Bingo!")
+            login_info = 1
             break
     else:
-        f = open(logfile, "r")
-        for line in f.readlines():
-            # print(line)
-            user_file, pass_file = line.split()
-            # print(user_file)
-            # print(pass_file)
-            if user_file == name and pass_file == password:
-                print("Bingo!")
-                lock1_info = 1
-                break
-        else:
-            print("You name or password is errer!")
-            i += 1
-        f.close()
+        print("You name or password is errer!")
+        i += 1
+    f.close()
 else:
-    if i == 3 or lock_info == 1:
-        print(i)
-        print(lock_info)
+    if i == 3 and login_info == 0:
         f = open(lock, "a")
         f.write(name + "\n")
         f.close()
-        print("This %s is locked" % name)
+        print('\033[32:1m用户 %s 已经被锁定\033[0m' % name)
